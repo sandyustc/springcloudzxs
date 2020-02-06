071b55cd742b5140029be86a4cbfb3951e12fd5a
sandyustc <xueshengzhang81@163.com>
2020-02-06 19:43:10 +0800

    add

---
+++ springcloud2020/microservicecloud-api/src/main/java/com/ustc/springcloud/entities/Dept.java
@@ -0,0 +1,9 @@
+package com.ustc.springcloud.entities;
+
+import java.io.Serializable;
+
+public class Dept implements Serializable {
+    private Long deptno;
+    private String dname;
+    private String db_source;
+}
