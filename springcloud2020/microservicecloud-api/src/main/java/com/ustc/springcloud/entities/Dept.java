737522657699dc4f4c090642e55728b7a5f64b63
sandyustc <xueshengzhang81@163.com>
2020-02-06 22:13:27 +0800

    add  2

--- springcloud2020/microservicecloud-api/src/main/java/com/ustc/springcloud/entities/Dept.java
+++ springcloud2020/microservicecloud-api/src/main/java/com/ustc/springcloud/entities/Dept.java
@@ -1,9 +1,21 @@
 package com.ustc.springcloud.entities;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
 public class Dept implements Serializable {
     private Long deptno;
     private String dname;
     private String db_source;
+
+    public Dept(String dname) {
+        this.dname = dname;
+    }
 }
